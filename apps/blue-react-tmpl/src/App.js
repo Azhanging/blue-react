4ba294754a9957316883468115d161b2131b1589
Blue <azhanging@qq.com>
2019-09-18 17:21:33 +0800

    update

--- apps/blue-react-tmpl/src/App.js
+++ apps/blue-react-tmpl/src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ActivityIndicator } from 'antd-mobile';
+
 
 //主App节点
-function App(props){
+function App(props) {
+
+  const [animating, setAnimation] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAnimation(!animating);
+    }, 2000);
+  }, [animating]);
+
   return (
     <>
       {props.children}
+      {/*loading*/}
+      <button className="bc-btn" onClick={() => setAnimation(!animating)}>
+        toggle animating
+      </button>
+      <ActivityIndicator toast text="loading" animating={animating}/>
     </>
   )
 }
